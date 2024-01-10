25280e58222772d41e275deff2970e8bae3468da
James King <james.king020773@gmail.com>
2024-01-10 15:52:31 +1100

    Initial python files

---
+++ Hello World.py
@@ -0,0 +1,14 @@
+print("Hello World")
+# Variable Declaration - Python does not require var type declarations
+a = 28 # assigning an whole number value (integer) 
+b = 1.5 # assigning a decimal value (float)
+c = "Hello" # assigning a text value (string)
+d = True # assigning a binary value (Boolean)
+e = None  # assigning the absence of a return value (NoneType)
+
+#Accepting Text Input
+name = input("Name: ")
+
+#Maniputlating Values
+print("Hello, " + name)
+print(f"Hello, {name}")
