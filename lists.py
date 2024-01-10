d3ba50a233aa24375523a315aa5bdef7e92a8431
James King <151979414+jamesking020773@users.noreply.github.com>
2024-01-10 16:49:22 +1100

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,14 +1,31 @@
-#String (Character Array)
-name = "Harry"
-print(name[0])
+list1=[1,7,3,'a','x','p',4.6,0.2,3.7]
+list1.append(2.3)
+list1
+list1.insert(2,0.9)
+list1
+list1.pop()
+list1
+list1.remove('x')
+list1
+list1.pop(4)
+list1
+len(list1)
 
-# Lists (Arrays)
-names = ["Harry", "Ron", "Hermione", "Ginny"]
-print(names)
-print(names[0])
+list2=[1,5,3,9,0]
+list2.sort()
+list2
+list2.index(3)
+list2.append(3)
+list2.count(3)
+list2.sort(reverse=True)
+list2
 
-names.append("Draco")
-print(names)
-
-names.sort()
-print(names)
+# Common built-in functions in Python for lists.
+#1. append(): This function is used to add an element to the list at the end.
+#2. insert(): This function takes the element and the index as arguments and inserts the value in the specified location.
+#3. pop(): This function deletes the last element if no index passes or deletes the element at that index and returns it.
+#4. remove(): This function removes the specified value from the list.
+#5. len(): This function returns the length or number of elements in the list
+#6. sort(): This arranges the elements of the list in ascending or descending order
+#7. index(): This is used to find the index of the element passed
+#8. count(): This finds the number of times the value occurs in a list
