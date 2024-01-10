1cf79fd2b33f8063f1c962d1aa608203bb7fdb92
James King <james.king020773@gmail.com>
2024-01-10 16:33:53 +1100

    More initial files

--- lists.py
+++ lists.py
@@ -1,7 +1,14 @@
-# Lists
+#String (Character Array)
+name = "Harry"
+print(name[0])
+
+# Lists (Arrays)
 names = ["Harry", "Ron", "Hermione", "Ginny"]
 print(names)
 print(names[0])
+
 names.append("Draco")
+print(names)
+
 names.sort()
 print(names)
