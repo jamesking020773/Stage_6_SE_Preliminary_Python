e399b90ddf6fef08acc1c5cc3c1ee71cd1cc936f
James King <151979414+jamesking020773@users.noreply.github.com>
2024-02-17 09:15:27 +1100

    Update flight.py

--- flight.py
+++ flight.py
@@ -4,16 +4,16 @@ class Flight():
         self.passengers = []
 
     def add_passenger(self, name):
-        if not self.open_seats():  #using 'not' is eqivalent to 'self.open_seats == 0'
+        if not self.open_seats():  # using 'not' is eqivalent to 'self.open_seats == 0'
             return False
         self.passengers.append(name)
         return True
 
     def open_seats(self):
-        return self.capacity - len(self.passengers) #Total capacity set when the flight instance was created - length of current passenger array
+        return self.capacity - len(self.passengers) # length of current passenger array
 
 
-flight = Flight(3) #Creates a flight instance and assigns the capacity variable the value of 3
+flight = Flight(3) # Creates a flight instance and assigns the capacity variable the value of 3
 
 people = ["Harry", "Ron", "Hermione", "Ginny", "James"]
 for person in people:
