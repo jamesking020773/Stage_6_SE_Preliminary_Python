5c1df20ed209cf734e98b85216faae0b1d50be3f
James King <151979414+jamesking020773@users.noreply.github.com>
2024-02-17 09:12:07 +1100

    Update square.py

--- square.py
+++ square.py
@@ -3,7 +3,4 @@ from functions import square
 for i in range(10):
     print(f"The square of {i} is {square(i)}")
 
-
-# import functions
-# for i in range(10):
-#   print(f"The square of {i} is {functions.square(i)}")
+# f is formatted text which means we can directly insert variables & calculations into the string
