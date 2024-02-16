a1d33ff5a8a99f6399ffaf6d0b34a8640a1ba86a
James King <151979414+jamesking020773@users.noreply.github.com>
2024-02-17 09:38:29 +1100

    Update decorators.py

--- decorators.py
+++ decorators.py
@@ -1,12 +1,16 @@
-def announce(f):
+def authorised(f):
     def wrapper():
-        print("About to run the function...")
-        f()
-        print("Done running the function.")
+        logged_in = False
+        print(f"User Login Status: {logged_in}")
+        if (logged_in):
+            return f()
+        else:
+            print("Error: User is not logged in")
     return wrapper
 
-@announce
-def hello():
+@authorised
+def hello(): # Only authorised users should see this message
     print("Hello world!")
 
-hello()
+hello() # When you call hello, it's wrapped by authorised.
+# By placing @authorised above the function definition, it's the same as saying hello = authorised(hello)
